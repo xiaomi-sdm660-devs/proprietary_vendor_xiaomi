40d5d7fb185444e5a98703044ef3cf3b2ada2a4e
Artem Borisov <dedsa2002@gmail.com>
2019-10-03 18:53:09 +0530

    sm6150-common: Update QTI bluetooth stack
    * Add new bluetooth audio blobs
    * Also add libbtconfigstore required by FM
    
    Change-Id: I99272538358b9a437ca87d09b94de2a0f9d6e1c6

--- davinci/Android.mk
+++ davinci/Android.mk
@@ -18,16 +18,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),davinci)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := libaudioparsers
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libaudioparsers.so
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libaudioparsers.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
 endif
