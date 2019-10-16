1f4bd86f56ac8a947c272e1883304e4c8a1d2fbf
Davide Garberi <dade.garberi@gmail.com>
2019-10-16 14:19:59 +0200

    sdm660-common: Regenerate vendor makefiles and blueprints

--- twolip/Android.mk
+++ twolip/Android.mk
@@ -18,16 +18,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),twolip)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := FingerprintExtensionService
-LOCAL_MODULE_OWNER := xiaomi
-LOCAL_SRC_FILES := proprietary/vendor/app/FingerprintExtensionService/FingerprintExtensionService.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_PREBUILT)
-
 endif
