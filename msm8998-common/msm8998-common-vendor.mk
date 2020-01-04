62b53564d324d0910502c08969ad08a7186ae191
Michael Bestas <mkbestas@lineageos.org>
2020-01-04 04:50:46 +0200

    msm8998-common: Add back uceShimService.apk
    * Regenerate makefiles while we are at it
    
    Change-Id: Ided808ec62575d513e5849df2071b294eff3cdee

--- msm8998-common/msm8998-common-vendor.mk
+++ msm8998-common/msm8998-common-vendor.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -578,6 +578,7 @@ PRODUCT_PACKAGES += \
     libantradio \
     libsdm-disp-vndapis \
     SoterService \
+    uceShimService \
     CNEService \
     WfdService \
     dpmserviceapp \
